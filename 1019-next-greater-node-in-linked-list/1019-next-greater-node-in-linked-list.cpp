8c4e73e53b484a1fbf891bea80601e9b9b870cef
Parijain718 <2303031550025@paruluniversity.ac.in>
2026-07-06 00:32:53 +0530

    Time: 8 ms (45.58%), Space: 47.5 MB (18.26%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> nums;
+        while(head){
+            nums.push_back(head->val);
+            head = head->next;
+        }
+        vector<int> ans(nums.size(),0);
+        stack<int> st;
+        for(int i = 0;i<nums.size();i++){
+            while(!st.empty() && nums[i] > nums[st.top()]){
+                ans[st.top()] = nums[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};

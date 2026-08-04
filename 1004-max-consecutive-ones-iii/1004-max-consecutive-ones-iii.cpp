f9b3b8977fe0af50b215fe5460a83da84c55ea22
Parijain718 <2303031550025@paruluniversity.ac.in>
2026-08-04 13:28:27 +0530

    Time: 0 ms (100%), Space: 69.5 MB (65.21%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,7 +4,7 @@ public:
         int left = 0;
         int zeros = 0;
         int maxLen = 0;
-        for(int right = 0;right < nums.size();right++){
+        for(int right = 0; right < nums.size(); right++){
             if(nums[right] == 0) zeros++;
             while(zeros > k){
                 if(nums[left] == 0) zeros--;

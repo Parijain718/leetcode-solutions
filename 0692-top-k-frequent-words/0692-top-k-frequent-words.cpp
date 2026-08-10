37d121d265debbfcec4f6f32fa5f2f64e5027724
Parijain718 <2303031550025@paruluniversity.ac.in>
2026-08-10 16:44:44 +0530

    Time: 0 ms (100%), Space: 17.2 MB (91.24%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int> mp;
+        for(string word : words){
+            mp[word]++;
+        }
+        vector<pair<string,int>> arr;
+        for(auto &it :mp){
+            arr.push_back({it.first,it.second});
+        }
+        sort(arr.begin(),arr.end(),[](auto &a,auto &b){
+            if(a.second != b.second){
+                return a.second > b.second;
+            }
+            return a.first < b.first;
+        });
+        vector<string> ans;
+        for(int i = 0; i < k; i++){
+            ans.push_back(arr[i].first);
+        }
+        return ans;
+    }
+};
